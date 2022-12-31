233048461f0118e137f3f9646a8f60e0e59b7cff
Kauan Vaz <kauanvazc@gmail.com>
2022-12-30 23:38:47 -0300

    Questions 2, 3, 4 and 5

---
+++ main.py
@@ -0,0 +1,79 @@
+import utils
+
+################ QUESTÃO 2 ################
+# ITEM A
+
+lena_gray = utils.Histogram()
+lena_gray.read_image('imgs/lena_gray.bmp')
+"""
+hist_lena_gray = lena_gray.calcHistogram()
+lena_gray.show(hist_lena_gray, "Histograma - lena_gray", "Intensidades", "Frequências")
+
+# ITEM B
+normal_hist_lena_gray = lena_gray.calcNormalizedHistogram()
+lena_gray.show(normal_hist_lena_gray, "Histograma normalizado - lena_gray", "Intensidades", "Probabilidades")
+
+# ITEM C
+acum_hist_lena_gray = lena_gray.calcAcumulatedHistogram()
+lena_gray.show(acum_hist_lena_gray, "Histograma acumulado - lena_gray", "", "Frequência acumulada")
+
+"""
+################ QUESTÃO 3 ################
+
+# ITEM A
+
+image1 = utils.Histogram()
+
+image1.read_image('imgs/image1.png')
+
+hist_image1 = image1.calcHistogram()
+image1.show(hist_image1, "Histograma - image1", "Intensidades", "Frequências")
+
+lena_eqHist, lena_eqImg, _ = lena_gray.calcEqualizedHistogram()
+img1_eqHist, img1_eqImg, _ = image1.calcEqualizedHistogram()
+
+lena_gray.show(lena_eqHist, "Histograma equalizado - lena_gray", "Intensidades", "Frequências")
+image1.show(img1_eqHist, "Histograma equalizado - image1", "Intensidades", "Frequências")
+
+util = utils.Util()
+
+util.save("./results/lena_equalizada.bmp", lena_eqImg)
+util.save("./results/image1_equalizada.png", img1_eqImg)
+
+# ITEM B
+
+lena_gray.read_image('results/lena_equalizada.bmp')
+image1.read_image('results/image1_equalizada.png')
+
+lena_eqHist, lena_eqImg, _ = lena_gray.calcEqualizedHistogram()
+img1_eqHist, img1_eqImg, _ = image1.calcEqualizedHistogram()
+
+lena_gray.show(lena_eqHist, "Histograma equalizado - lena_equalizada", "Intensidades", "Frequências")
+image1.show(img1_eqHist, "Histograma equalizado - image1_equalizada", "Intensidades", "Frequências")
+
+util.save("./results/2_lena_equalizada.bmp", lena_eqImg)
+util.save("./results/2_image1_equalizada.png", img1_eqImg)
+
+################ QUESTÃO 4 ################
+
+lt = utils.LinearTransformations()
+
+lt.read_image('imgs/lena_gray.bmp')
+
+linear = lt.calcLinear(0.5, 10)
+loga = lt.calcLogarithm(16)
+exp = lt.calcExponential(1.2)
+
+util.save("./results/4linear.png", linear)
+util.save("./results/4logarithm.png", loga)
+util.save("./results/4exponential.png", exp)
+
+################ QUESTÃO 5 ################
+
+lena_gray.read_image('imgs/lena_gray.bmp')
+
+lena_hist = lena_gray.calcHistogram()
+hist, img = image1.calcSpecifiedHistogram(lena_hist)
+
+image1.show(hist, "Especificação de image1.png com lena_gray", "Intensidades", "Frequências")
+util.save("./results/image1_com_lena.png", img)
---
+++ utils.py
@@ -0,0 +1,150 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Util:
+    def save(self, dir, data):
+        cv2.imwrite(dir, data)
+
+class Histogram:
+    def __init__(self):
+        self.flattened_image = np.array([])
+        self.shape = ()
+        self.totalPixels = 0
+        self.intensidades_labels = [str(x) for x in range(256)]
+
+    def read_image(self, dir_img):
+        original_img = cv2.imread(dir_img, cv2.IMREAD_GRAYSCALE)
+        self.shape = original_img.shape
+        self.totalPixels = original_img.size
+        self.flattened_image = np.ravel(original_img)
+
+    def calcHistogram(self):
+        frequencies = [0]*256
+        for p in self.flattened_image:
+            frequencies[p] += 1
+
+        return frequencies
+
+    def calcNormalizedHistogram(self):
+        normalized_freqs = [0]*256
+        freqs = self.calcHistogram()
+        for i,v in enumerate(freqs):
+            normalized_freqs[i] = float(v)/self.totalPixels
+
+        return normalized_freqs
+
+    def calcAcumulatedHistogram(self):
+        acumulated_hist = [0]*256
+        normal_hist = self.calcNormalizedHistogram()
+
+        for i, v in enumerate(normal_hist):
+            acumulated_hist[i] = v if i == 0 else acumulated_hist[i-1] + v
+
+        return acumulated_hist
+
+    def calcEqualizedHistogram(self):
+
+        freqs = self.calcHistogram()
+
+        intens_max = len(freqs)-1
+        equalized_hist = [0]*(intens_max+1)
+        img_size = len(self.flattened_image)
+        equalized_flat_image = np.array([0]*img_size)
+        mapping = []
+
+        for i, v in enumerate(freqs):
+            aux = [intens_max*(freqs[x])/self.totalPixels for x in range(i+1)]
+            novo_valor = round(sum(aux))
+
+            equalized_hist[novo_valor] = freqs[i] if equalized_hist[novo_valor] == 0 else equalized_hist[novo_valor] + freqs[i]
+            mapping.append(novo_valor)
+
+            aux_image = np.array([0]*img_size)
+            indexes = np.where(self.flattened_image == i)
+
+            for i in indexes[0]: aux_image[i] = novo_valor
+
+            equalized_flat_image = np.add(equalized_flat_image, aux_image)
+
+        equalized_image = equalized_flat_image.reshape(self.shape)
+        mapping = np.array(mapping)
+        return equalized_hist, equalized_image, mapping
+
+    def __find_closest(self, arr, val): # Return index
+       idx = np.abs(arr - val).argmin()
+       return idx
+
+    def __transformationFunction(self, values):
+        dir = "./figures/"
+        plt.plot(self.intensidades_labels, values)
+        plt.xticks(np.arange(0, len(values)+1, 50))
+        plt.savefig(dir + "Funcao_transformacao" + ".png", format="png")
+        plt.show()
+
+    def calcSpecifiedHistogram(self, z):
+        eq_hist, _, eq_mapping = self.calcEqualizedHistogram()
+
+        L = 256
+        specified_hist = [0]*L
+        spec_mapping = []
+
+        for i, v in enumerate(z):
+            aux = [(L-1)*(z[x])/self.totalPixels for x in range(i+1)]
+            novo_valor = round(sum(aux))
+
+            specified_hist[novo_valor] = z[i] if specified_hist[novo_valor] == 0 else specified_hist[novo_valor] + z[i]
+            spec_mapping.append(novo_valor)
+        
+        self.__transformationFunction(spec_mapping)
+        
+        new = []
+        for i,v in enumerate(eq_mapping):
+            closest = self.__find_closest(spec_mapping, v)
+            new.append(closest)
+
+
+        freqs = self.calcHistogram()
+        new_hist = [0]*L
+        new_flat_image = np.array([0]*len(self.flattened_image))
+
+        for i, v in enumerate(freqs):
+            new_hist[new[i]] = freqs[i] if new_hist[new[i]] == 0 else new_hist[new[i]] + freqs[i]
+
+            aux_image = self.flattened_image.copy()
+            aux_image[aux_image!=i] = 0
+            aux_image[aux_image==i] = new[i]
+
+            new_flat_image = np.add(new_flat_image, aux_image)
+
+        new_image = new_flat_image.reshape(self.shape)
+        
+        return new_hist, new_image
+
+    def show(self, valores, title, xlabel, ylabel):
+        dir = "./figures/"
+        plt.bar(self.intensidades_labels, valores)
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.xticks(np.arange(0, len(valores)+1, 50))
+        plt.savefig(dir + title + ".png", format="png")
+        plt.show()
+
+class LinearTransformations:
+    def __init__(self):
+        self.img = np.array([])
+
+    def read_image(self, dir_img):
+        self.img = cv2.imread(dir_img, cv2.IMREAD_GRAYSCALE)
+    
+    def calcLinear(self, c, b):
+        return np.array(c*self.img+b, dtype = np.uint8) # float to int
+
+    def calcLogarithm(self, c):
+        log_image = c*np.log2(self.img+1)
+        return np.array(log_image, dtype = np.uint8) # float to int
+
+    def calcExponential(self, c):
+        exp_image = c*np.exp((self.img+1)/(255+1))*(255/np.exp(1))
+        return np.array(exp_image, dtype = np.uint8) # float to int
